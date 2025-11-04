7d22966867c15dd6fa50d9807ccea51ed5e1a0a7
s-di-cola <admin@b-side.dev>
2025-11-04 16:11:11 +0000

    fix: use 'config apply --force' without unnecessary deps install

--- juno.config.ts
+++ juno.config.ts
@@ -7,6 +7,8 @@ export default defineConfig({
       preview: "64njw-oiaaa-aaaal-asppa-cai",
       production: "dkk74-oyaaa-aaaal-askxq-cai"
     },
+    source: "build",
+    predeploy: ["pnpm run build"],
     collections: {
       datastore: [
         {
